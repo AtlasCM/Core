07da706913d62083b81966023b90d1f6178c2591
CupOfTea696 <cupoftea696@gmail.com>
2015-10-23 12:05:52 +0100

    welp, missed this one

--- src/Installer/routes.php
+++ src/Installer/routes.php
@@ -1,3 +1,3 @@
 <?php
 
-Route::get('/', ['as' => 'home', 'uses' => '\Atlas\Core\Installer\Http\Controllers\InstallerController@welcome']);
+Route::get('/', ['as' => 'home', 'uses' => '\Atlas\Installer\Http\Controllers\InstallerController@welcome']);
